2e8c3a402b12f3db262e623506841d3608ae9973
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-16 15:01:26 +0530

    ADDED : Decide Player Symbol By Using Random Function

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -8,4 +8,16 @@ Arr=(. . . . . . . . .)
 	echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
 	echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
 
+function getAssignedLetter()
+{
 
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+			echo "Player 1 Symbol X"
+	else
+			echo "Player 2 Symbol 0"
+	fi
+
+}
+
+getAssignedLetter
