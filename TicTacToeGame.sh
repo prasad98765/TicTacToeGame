9b02eb80097076441d861ff7bb828e58f20feffe
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-16 12:31:10 +0530

    ADDED : To Initialized The board

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "WECLOME : To TicTacToe Game"
+
+Arr=(. . . . . . . . .)
+	echo "r\c 0 1 2"
+	echo "0   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+	echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+	echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+
+
