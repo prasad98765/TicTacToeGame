3223392800dbe848131665175996d5003d566c8a
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-18 09:57:47 +0530

    ADDED : Functionality to check if the player won

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -2,29 +2,83 @@
 
 echo "WECLOME : To TicTacToe Game"
 
-	gameBoard=(1 2 3 4 5 6 7 8 9)
-	echo "r\c 0   1   2"
-
-	echo "0   ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}"
-	echo 		"---------------"
-	echo "1   ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}"
-	echo 		"---------------"
-	echo "2   ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}"
-
-
-
-function getAssignedLetter()
+declare -a gameBoard
+gameBoard=(1 2 3 4 5 6 7 8 9)
+count=0
+function toPrintBoard()
 {
 
-	if [ $((RANDOM%2)) -eq 1 ]
-	then
-			echo "player 1 "
-			echo "Player Symbol X"
-	else
-			echo "player 2 "
-			echo "Player Symbol x"
+	echo "  -----------"
+   echo "   ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}"
+   echo "  -----------"
+   echo "   ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}"
+   echo "  -----------"
+   echo "   ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}"
+	echo "  -----------"
+   toAssigningRowColumnDiagonalValue
+   getPlayerInput
+
+}
+function toAssigningPlayerSymbol()
+{
+
+   if [ $((RANDOM%2)) -eq 1 ]
+   then
+         echo "player 1 play First"
+         echo "Player Symbol X"
+   else
+         echo "player 1 play first"
+         echo "Player Symbol x"
+   fi
+   toPrintBoard
+
+}
+
+function getPlayerInput()
+{
+
+   while [[ $count -lt 9 ]]
+   do
+      echo "Enther the Valid Number beween 1 to 8"
+      read number
+      count=$(( $count + 1 ))
+      gameBoard[$number -1]="X"
+      toPrintBoard
+   done
+
+}
+
+function toCheckMatchOrNot()
+{
+
+   if [ ${gameBoard[$1]} == ${gameBoard[$2]} ] && [ ${gameBoard[$2]} == ${gameBoard[$3]} ]; 
+   then
+      echo "win"
+      exit
 	fi
 
 }
-getAssignedLetter
+
+function toAssigningRowColumnDiagonalValue()
+{
+
+  toCheckMatchOrNot 0 1 2
+  toCheckMatchOrNot 3 4 5
+  toCheckMatchOrNot 6 7 8
+  toCheckMatchOrNot 0 3 6
+  toCheckMatchOrNot 1 4 7
+  toCheckMatchOrNot 2 5 8
+  toCheckMatchOrNot 0 4 8
+  toCheckMatchOrNot 2 4 6
+
+}
+
+function main()
+{
+
+   toAssigningPlayerSymbol
+
+}
+
+main
 
