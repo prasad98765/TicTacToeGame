4fa73385489a94feac02106649d9e298274c4da8
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-19 10:34:47 +0530

    ADDED : Function Check Row Column Diagonal for Winning position

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -2,48 +2,105 @@
 
 echo "WECLOME : To TicTacToe Game"
 
+#CONSTANT
+
+PLAYER_LETTER=""
+COMPUTER_LETTER=""
+
 declare -a gameBoard
+
+#VARIABLE
 gameBoard=(1 2 3 4 5 6 7 8 9)
 count=0
-function toPrintBoard()
+flag=0
+
+function printBoard()
 {
 
-	echo "  -----------"
+   echo "               "
    echo "   ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}"
-   echo "  -----------"
+   echo "  -------------"
    echo "   ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}"
-   echo "  -----------"
+   echo "  -------------"
    echo "   ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}"
-	echo "  -----------"
+   echo "               "
    toAssigningRowColumnDiagonalValue
-   getPlayerInput
 
 }
-function toAssigningPlayerSymbol()
+function getAssignedSymbol()
 {
 
    if [ $((RANDOM%2)) -eq 1 ]
    then
-         echo "player 1 play First"
-         echo "Player Symbol X"
+      PLAYER_LETTER="X"
+      COMPUTER_LETTER="O"
+      echo "Player Letter" $PLAYER_LETTER
+      echo "Computer Letter" $COMPUTER_LETTER
+      echo "player 1"
    else
-         echo "player 1 play first"
-         echo "Player Symbol x"
+      PLAYER_LETTER="0"
+      COMPUTER_LETTER="X"
+      echo "Player Letter" $PLAYER_LETTER
+      echo "Computer Letter" $COMPUTER_LETTER
+      echo "player 2"
    fi
-   toPrintBoard
+   getUserComputerInput
 
 }
 
-function getPlayerInput()
+function getUserInput()
 {
 
+   echo "User Play"
+   echo "Enter the Valid Number beween 0 to 8"
+   read number
+   for (( i=0; i<9 ;i++ ))
+   do
+      if [[ ${gameBoard[$i]} -eq $number ]]
+      then
+         gameBoard[$number -1]=$PLAYER_LETTER
+         ((count++))
+         flag=1
+         printBoard
+      fi
+   done
+
+}
+function getComputerInput()
+{
+
+   echo "Computer Play"
+   random=$(( RANDOM%8+1 ))
+   for (( i=0; i<9; i++ ))
+   do
+      if [[ ${gameBoard[$i]} -eq $random ]]
+      then
+         gameBoard[$random -1]=$COMPUTER_LETTER
+         ((count++))
+         flag=0
+         printBoard
+         break
+      fi
+      flag=1
+   done
+
+}
+
+function getUserComputerInput()
+{
+
+   printBoard
    while [[ $count -lt 9 ]]
    do
-      echo "Enther the Valid Number beween 1 to 8"
-      read number
-      count=$(( $count + 1 ))
-      gameBoard[$number -1]="X"
-      toPrintBoard
+      if [[ $flag -eq 0 ]]
+      then
+         getUserInput
+      fi
+
+      if [[ $flag -eq 1 ]]
+      then
+         getComputerInput
+      fi
    done
 
 }
@@ -51,11 +108,16 @@ function getPlayerInput()
 function toCheckMatchOrNot()
 {
 
-   if [ ${gameBoard[$1]} == ${gameBoard[$2]} ] && [ ${gameBoard[$2]} == ${gameBoard[$3]} ]; 
+   if [ ${gameBoard[$1]} == ${gameBoard[$2]} ] && [ ${gameBoard[$2]} == ${gameBoard[$3]} ] 
    then
-      echo "win"
-      exit
-	fi
+      if [[ $flag -eq 1 ]]
+      then
+               echo "User.... Win"
+      else
+                  echo "Computer.... Win"
+      fi
+       exit
+   fi
 
 }
 
@@ -76,7 +138,7 @@ function toAssigningRowColumnDiagonalValue()
 function main()
 {
 
-   toAssigningPlayerSymbol
+   getAssignedSymbol
 
 }
 
